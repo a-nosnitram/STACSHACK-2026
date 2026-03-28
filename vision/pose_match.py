c7ad16517c9151b6ad2670652777a17c0ac130f3
Soph <sofia.morgulchik@gmail.com>
2026-03-28 17:35:16 +0000

    keep torso flat in plank

--- vision/pose_match.py
+++ vision/pose_match.py
@@ -1,23 +1,4 @@
 from vision.plank import plank_match
-# from vision.pose_utils import (
-#     LA,
-#     LH,
-#     LK,
-#     LS,
-#     RA,
-#     RH,
-#     RK,
-#     RS,
-#     LW,
-#     RW,
-#     angle_deg,
-#     get,
-#     presence,
-#     score_above,
-#     score_below,
-#     score_below_abs,
-#     xy,
-# )
 from vision.squat import squat_match
 
 def match_expected_pose(
