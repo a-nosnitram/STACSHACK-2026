1cf95af7ecbf1f2e35f7f4be636d75ec7f68e544
a-nosnitram <stasymartinson@gmail.com>
2026-03-29 02:41:46 +0100

    chore(constants): remove def chars

--- game/constants.py
+++ game/constants.py
@@ -4,9 +4,3 @@ poses = ["squat", "bear", "plank", "bug", "lunge"]
 # screen settings
 screen_width = 1400
 screen_height = 700
-
-
-selected_characters = {
-    "left": "soph",
-    "right": "yehor",
-}
