c605ae2e675c7f4c09c865454f5449e99eb912ac
a-nosnitram <stasymartinson@gmail.com>
2026-03-29 02:36:34 +0100

    fix(poses): fix combos

--- game/utils.py
+++ game/utils.py
@@ -1,22 +1,13 @@
+from game.constants import poses as available_poses
+
 
 def pose_combo(poses):
     if len(poses) == 5:
         return poses
     else:
-        # fill the rest with random poses
+        # fill the rest with repeated poses if there are not enough unique ones, to ensure we always send 5 poses to vision
         import random
 
-        all_poses = [
-            "squat",
-            "lunge",
-            "dead_bug",
-            "kick",
-            "dab",
-            "dab_reverse",
-            "tree",
-            "warrior2_left",
-            "warrior2_right",
-        ]
-        remaining_poses = [p for p in all_poses if p not in poses]
+        remaining_poses = [p for i in range(5 - len(poses)) for p in poses]
         random.shuffle(remaining_poses)
         return poses + remaining_poses[: (5 - len(poses))]
